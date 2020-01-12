87c3fb22539dcbaabb4362e32f588c9f7519c4dd
BlackCoffeeCat <allen1995.cngz@gmail.com>
2020-01-13 06:15:27 +0800

    change machine hostname after dns config success.

--- init.sh
+++ init.sh
@@ -30,6 +30,7 @@ install_redis() {
     REDIS_PWD=$(pwgen 24 -1)
     echo "requirepass $REDIS_PWD" | sudo tee -a /etc/redis/redis.conf
     echo "bind 0.0.0.0" | sudo tee -a /etc/redis/redis.conf
+    sudo systemctl restart redis
     gcloud compute project-info add-metadata --metadata "${HOSTNAME}_REDIS_PWD=$REDIS_PWD"
   fi
 }
@@ -59,6 +60,16 @@ if [ ! -z "$host" ]; then
     echo "CF_Email=$CF_Email" | sudo tee -a /etc/envirement
     echo "CF_Key=$CF_Key" | sudo tee -a /etc/envirement
     install_node
-    curl https://sh.open51.net/cf-dns.js | node - "$host" "$(curl icanhazip.com)" "$CF_Email" "$CF_Key"
+    MY_ADDR=$(curl icanhazip.com);
+    curl https://sh.open51.net/cf-dns.js | node - "$host" "$MY_ADDR" "$CF_Email" "$CF_Key"
+    sleep 20
+    if [ -z "$(command -v dig)" ]; then
+      sudo apt-get install -y dnsutils
+    fi
+    $ADDR=$(dig +short "$host" 1.1.1.1 | head -1)
+
+    if [ "$MY_ADDR" == "$ADDR" ]; then
+      sudo hostname "$host"
+    fi
   fi
 fi
